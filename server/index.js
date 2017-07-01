1c90cb61322370561316744c936b52754be47290
Nguyen Tien Nam <nguyennampfiev1995@gmail.com>
2017-07-01 14:47:38 +0700

    View list user

---
+++ server/index.js
@@ -0,0 +1,18 @@
+var io =  require("socket.io")(3000,function () {
+    console.log("Start server");
+});
+var arrPeerID =[];
+io.on("connection",function (socket) {
+    console.log(socket.id);
+    socket.emit("Online-Peer",arrPeerID);
+    socket.on("New-peer-id",function (peerID) {
+        socket.idPeer= peerID;
+        arrPeerID.push(peerID);
+        io.emit("New_Client_Connect",peerID);
+    })
+    socket.on("disconnect",function () {
+        var index  = arrPeerID.indexOf(socket.idPeer);
+        arrPeerID.splice(index,1);
+        io.emit("Someone_Disconnected",socket.idPeer);
+    })
+})
--- src/app.js
+++ src/app.js
@@ -3,13 +3,17 @@ var uid = require("uid");
 var $ =require("jquery");
 var open = require("./getStream");
 var  playVideo =require("./playVideo");
+var io =require("socket.io-client");
+var socket =io("http://localhost:3000");
 var config = {host:"stream3006.herokuapp.com",port:443,secure:true};
 function getPeer() {
     var id=uid(10);
     $("#peer-id").append(id);
     return id;
 }
-var peer = new Peer(getPeer(),config);
+var peerID = getPeer();
+socket.emit("New-peer-id",peerID);
+var peer = new Peer(peerID,config);
 $("#btnCall").click(function () {
     var friendid =$('#txtFriend-id').val();
     open(function (stream) {
@@ -26,9 +30,7 @@ $("#btnCall").click(function () {
     })
 })
 peer.on("call",function (call) {
-    console.log('123123123');
     open(function (Fstream) {
-        console.log('123123123');
         playVideo(Fstream,"localStream");
         call.answer(Fstream);
         call.on("stream",function (remoteStream) {
@@ -37,7 +39,37 @@ peer.on("call",function (call) {
             console.log(err);
         }
     })
+});
+socket.on("Online-Peer",function (arr) {
+ arr.forEach(function (PeerID) {
+     console.log(PeerID);
+     $("#ulPeerID").append(`<li id="${PeerID}">${PeerID}</li>`);
+
+ })
 })
+socket.on("New_Client_Connect",function (PeerID) {
+
+    $("#ulPeerID").append(`<li id="${PeerID}">${PeerID}</li>`);
+})
+socket.on("Someone_Disconnected",function (PeerID) {
+    $(`#${PeerID}`).remove();
+
+})
+$("#ulPeerID").on('click','li',function () {
+    var peerid = $(this).text();
+    open(function (stream) {
+        playVideo(stream,"localStream");
+        var call = peer.call(peerid, stream);
+        console.log("call  :"+call);
+        console.log("here");
+        call.on("stream",function (remoteStream) {
+            playVideo(remoteStream,"friendStream");
+        }),function (err) {
+            console.log(err);
+        }
+    })
+})
+
 console.log(peer);
 
 
